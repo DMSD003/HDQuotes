d1cc548236034bc8deabee6e87d0aa5ad621a105
Djombissi Daniels <mintsadaniels175@gmail.com>
2026-09-20 21:00:08 +0100

    New update on server port

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,'0.0.0.0' () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is listenning on port ${PORT}`);
 })
